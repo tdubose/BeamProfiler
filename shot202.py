83bf6fdc7d1cfdc84d8bab35fe3d0bd74b2b1c98
Theo DuBose <tdubose@gmail.com>
2014-06-29 20:01:34 -0400

    Added empty class file for SHOT-202 RS232 interface. The functions should be filled out so that they perform the tasks described in comments.

---
+++ shot202.py
@@ -0,0 +1,11 @@
+import pyvisa
+ 
+class Shot202():
+ 
+    def __init__(self, port):
+	
+	def go(pos): # goes to pos in pulses
+	
+	def go_mm(pos): # goes to pos in mm
+	
+	def getpos(inpulses=False): # gets position, returns in pulses if inpulses is True
